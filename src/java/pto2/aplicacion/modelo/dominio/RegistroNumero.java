66597e4a425cec4d88f8921ff216b5f300bdad21
Alumno <Alumno@2CIDC85DE6359BC>
2018-05-04 21:44:25 -0300

    Clase RegistroNumero Punto2

--- src/java/pto2/aplicacion/modelo/dominio/RegistroNumero.java
+++ src/java/pto2/aplicacion/modelo/dominio/RegistroNumero.java
@@ -5,25 +5,62 @@
  */
 package pto2.aplicacion.modelo.dominio;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author E. Misael Ortiz
  */
 public class RegistroNumero {
-    private double n;
+    ArrayList <Integer> registroNum = new ArrayList<Integer>();
+    int numIngresado;
 
-    /**
-     * @return the n
-     */
-    public double getN() {
-        return n;
+    public int getNumIngresado() {
+        return numIngresado;
     }
 
-    /**
-     * @param n the n to set
-     */
-    public void setN(double n) {
-        this.n = n;
+    public void setNumIngresado(int numIngresado) {
+        this.numIngresado = numIngresado;
+    }
+
+    public void ingresarNumero (){
+        registroNum.add(numIngresado);
+}
+    public int visualizarNumeros(){
+        return registroNum.get(0);
+    }
+    public int calcularMaximo (){
+        int numMayor = 0;
+        for (int i=0; i<registroNum.size();i++){
+            if(registroNum.get(i)>numMayor){
+                numMayor = registroNum.get(i);
+            }
+        }
+        return numMayor;
+    }
+    public int calcularMenor(){
+        int numMenor = registroNum.get(0);
+        for (int i=0; i<registroNum.size();i++){
+            if(registroNum.get(i)<numMenor){
+                numMenor = registroNum.get(i);
+            }
+        }
+        return numMenor;
+    }
+    public double calcularPromedio(){
+        int suma = 0;
+        for(int i=0;i<registroNum.size();i++){
+            suma=suma+registroNum.get(i);
+        }
+        return suma / registroNum.size();
+    }
+
+    public ArrayList<Integer> getRegistroNum() {
+        return registroNum;
+    }
+
+    public void setRegistroNum(ArrayList<Integer> registroNum) {
+        this.registroNum = registroNum;
     }
     
 }
