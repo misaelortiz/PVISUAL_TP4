fac0c382264c924fd8503b1b0cd0423533367629
Alumno <Alumno@2CIDC85DE6359BC>
2018-05-07 14:31:43 -0300

    coche pto6

---
+++ src/java/pto6/aplicacion/modelo/dominio/Coche.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pto6.aplicacion.modelo.dominio;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Coche {
+    private String patente;
+    private String marca;
+    private String modelo;
+    private String color;
+    private String combustible;
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+    
+    
+    
+   public void mostrarAutos(Coche[] autos){
+        }
+   public void modificarValoresAuto(Coche[] coches, int i){
+        
+          
+}
+}
